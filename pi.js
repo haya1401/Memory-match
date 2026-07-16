701913415250e9c47e86815734ea5049e01dbbec
haya1401 <haua112233@icloud.com>
2026-07-16 13:36:30 +0300

    Add Pi initialization and login function
    Initialize Pi with version 2.0 and implement login function.

--- pi.js
+++ pi.js
@@ -1,1 +1,31 @@
+Pi.init({
+version:"2.0"
+});
 
+
+async function loginPi(){
+
+
+try{
+
+
+let user=await Pi.authenticate(
+["username"],
+()=>{}
+);
+
+
+document.getElementById("user").innerHTML=
+"مرحبا "+user.user.username;
+
+
+}
+
+catch(e){
+
+console.log(e);
+
+}
+
+
+}
